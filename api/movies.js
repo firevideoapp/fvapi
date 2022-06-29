d4a114ddd59b7d4324ba0bbc8a540e1571c73abb
firevideoapp <firevideoapp@gmail.com>
2022-06-29 23:05:05 +0530

    Update movies.js

--- api/movies.js
+++ api/movies.js
@@ -1,7 +1,8 @@
 const axios = require("axios").default;
 
 module.exports = async (req, res) => {
-    var reqQuery = req.query.query;
+    var reqQueryCategory = req.query.category;
+    var reqQueryVal = req.query.value;
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Cache-Control", "s-maxage=300, stale-while-revalidate")
     res.setHeader("Made-By", "FireVideo")
@@ -15,25 +16,29 @@ module.exports = async (req, res) => {
             var songRes = []
             
             for (i = 0; i < data.length; i++) {
-                songRes.push({
-                    dateAdded: data[i].dateAdded,
-                    provider: data[i].provider,
-                    tmdb: data[i].tmdb,
-                    adult: data[i].adult,
-                    contentId: data[i].contentId,
-                    movieName: data[i].movieName,
-                    movieLang: data[i].movieLang,
-                    movieImage: data[i].movieImage,
-                    movieArt: data[i].movieArt,
-                    movieGenre: data[i].movieGenre,
-                    keywords: data[i].keywords,
-                    movieStory: data[i].movieStory,
-                    movieUrl: data[i].movieUrl,
-                    drmLicense: data[i].drmLicense,
-                    sdServer: data[i].sdServer,
-                    hdServer: data[i].hdServer,
-                    fhdServer: data[i].fhdServer
-                })
+                if (reqQueryCategory == 'lg') {
+                    if (data[i].movieLang == reqQueryVal) {
+                        songRes.push({
+                            dateAdded: data[i].dateAdded,
+                            provider: data[i].provider,
+                            tmdb: data[i].tmdb,
+                            adult: data[i].adult,
+                            contentId: data[i].contentId,
+                            movieName: data[i].movieName,
+                            movieLang: data[i].movieLang,
+                            movieImage: data[i].movieImage,
+                            movieArt: data[i].movieArt,
+                            movieGenre: data[i].movieGenre,
+                            keywords: data[i].keywords,
+                            movieStory: data[i].movieStory,
+                            movieUrl: data[i].movieUrl,
+                            drmLicense: data[i].drmLicense,
+                            sdServer: data[i].sdServer,
+                            hdServer: data[i].hdServer,
+                            fhdServer: data[i].fhdServer
+                        })
+                    }
+                }
             }
 
             res.json(songRes)
