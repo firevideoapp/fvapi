4028680b1c0baca670258a93e0a4d9fda7741e25
firevideoapp <firevideoapp@gmail.com>
2022-06-29 23:12:14 +0530

    Update movies.js

--- api/movies.js
+++ api/movies.js
@@ -18,27 +18,26 @@ module.exports = async (req, res) => {
             for (i = 0; i < data.length; i++) {
                 if (reqQueryCategory === 'lg') {
                     if (reqQueryVal === data[i].movieLang) {
-                        alert("dsadsadsa")
+                        songRes.push({
+                            dateAdded: data[i].dateAdded,
+                            provider: data[i].provider,
+                            tmdb: data[i].tmdb,
+                            adult: data[i].adult,
+                            contentId: data[i].contentId,
+                            movieName: data[i].movieName,
+                            movieLang: data[i].movieLang,
+                            movieImage: data[i].movieImage,
+                            movieArt: data[i].movieArt,
+                            movieGenre: data[i].movieGenre,
+                            keywords: data[i].keywords,
+                            movieStory: data[i].movieStory,
+                            movieUrl: data[i].movieUrl,
+                            drmLicense: data[i].drmLicense,
+                            sdServer: data[i].sdServer,
+                            hdServer: data[i].hdServer,
+                            fhdServer: data[i].fhdServer
+                        })
                     }
-                    songRes.push({
-                        dateAdded: data[i].dateAdded,
-                        provider: data[i].provider,
-                        tmdb: data[i].tmdb,
-                        adult: data[i].adult,
-                        contentId: data[i].contentId,
-                        movieName: data[i].movieName,
-                        movieLang: data[i].movieLang,
-                        movieImage: data[i].movieImage,
-                        movieArt: data[i].movieArt,
-                        movieGenre: data[i].movieGenre,
-                        keywords: data[i].keywords,
-                        movieStory: data[i].movieStory,
-                        movieUrl: data[i].movieUrl,
-                        drmLicense: data[i].drmLicense,
-                        sdServer: data[i].sdServer,
-                        hdServer: data[i].hdServer,
-                        fhdServer: data[i].fhdServer
-                    })
                 }
             }
 
