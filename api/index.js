d0f1be3802d8cc15f172cb6664814904876c43e2
firevideoapp <firevideoapp@gmail.com>
2022-06-29 21:17:47 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,5 +6,10 @@ module.exports = async (req, res) => {
             documentation: "https://github.com/cachecleanerjeet/JiosaavnAPI/wiki",
             made_by: "https://github.com/cachecleanerjeet"
         })
+    } else {
+        res.json({
+            status: "Authentication Failed",
+            made_by: "https://github.com/cachecleanerjeet"
+        })
     }
 }
