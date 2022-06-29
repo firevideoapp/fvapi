a1a16433e2a619937db5785b9e4747bd6bccd68e
firevideoapp <firevideoapp@gmail.com>
2022-06-29 22:38:11 +0530

    Update movies.js

--- api/movies.js
+++ api/movies.js
@@ -6,21 +6,23 @@ module.exports = async (req, res) => {
     res.setHeader("Cache-Control", "s-maxage=300, stale-while-revalidate")
     res.setHeader("Made-By", "FireVideo")
 
-    axios.get('https://raw.githubusercontent.com/firevideoapp/fvdb/main/DB/MOVIES.json')
+    axios({
+        method: 'get',
+        url: `https://raw.githubusercontent.com/firevideoapp/fvdb/main/DB/MOVIES.json`
+    })
         .then(async function (response) {
             var data = JSON.parse(JSON.stringify(response.data))
             var songRes = []
             // for (i = 0; i < data.length; i++) {
-                
+
             // }
 
-            songRes.push({
-                id : data[0].contentId,
+            songRes.JSON({
+                id: data[0].contentId,
                 name: data[0].movieName
             })
-            res.JSON(songRes)
         })
         .catch(function (error) {
-            
-        });
+            res.json({ result: "false" })
+        })
 }
